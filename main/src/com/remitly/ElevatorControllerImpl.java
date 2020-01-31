f79ff8d3bd0c8fd8bdfc47e154f22684bcf4788c
mikeluoma <mike@remitly.com>
2020-01-31 12:26:54 +0100

    Shrink starter to test based project

--- main/src/com/remitly/ElevatorControllerImpl.java
+++ main/src/com/remitly/ElevatorControllerImpl.java
@@ -1,6 +1,25 @@
 package com.remitly;
-
 /**
+ * You are implementing an elevator.
+ *
+ * The elevator has 4 actions: (see ElevatorAction.java)
+ *
+ *     UP_ONE
+ *     DOWN_ONE
+ *     OPEN_CLOSE
+ *     NO_OP
+ *
+ * Additional Guidance:
+ *
+ *     This is unlike a normal elevator. Users request the elevator by pressing a single giant button - there's no up or down option.
+ *     Once on the elevator, users press a button corresponding to the floor they want to go to.
+ *
+ *     Assume that open/close is atomic (so people who need to get off will get off and people who need to get on will get on in the same action
+ *
+ *     Assume some process is calling getNextAction() and executing the action. You don't have to worry about how or when.
+ *
+ *     Assume that moving floors and opening / closing is instant and never fails. You don't have to worry about failing an action.
+ *
  * Implement your solution here. It's ok to maintain state in this controller.
  */
 public class ElevatorControllerImpl implements ElevatorController {
--- main/src/com/remitly/Main.java
+++ main/src/com/remitly/Main.java
@@ -1,66 +1,7 @@
 package com.remitly;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Main {
-    /**
-     * You are implementing an elevator.
-     *
-     * The elevator has 4 actions: (see ElevatorAction.java)
-     *
-     *     UP_ONE
-     *     DOWN_ONE
-     *     OPEN_CLOSE
-     *     NO_OP
-     *
-     * Please implement your solution in ElevatorControllerImpl.java
-
-     * Additional Guidance:
-     *
-     *     This is unlike a normal elevator. Users request the elevator by pressing a single giant button -- there's no up or down option.
-     *     Once on the elevator, users press a button corresponding to the floor they want to go to.
-     *
-     *     Assume that open/close is atomic (so people who need to get off will get off and people who need to get on will get on in the same action
-     *
-     *     Assume some process is calling getNextAction() and executing the action. You don't have to worry about how or when.
-     *
-     *     Assume that moving floors and opening / closing is instant and never fails. You don't have to worry about failing an action.
-     */
-
 
     public static void main(String[] args) {
-        int lowestFloor = getIntegerInput("Enter Lowest Floor");
-        int highestFloor = getIntegerInput("Enter Highest Floor");
-        if (lowestFloor >= highestFloor) {
-            System.out.println("Invalid elevator scenario.  Bye!");
-            System.exit(0);
-        }
-
-        ElevatorController controller = new ElevatorControllerImpl();
-
-        ElevatorRunner runner = new ElevatorRunner(controller);
-        runner.start();
-
-        ElevatorTrafficGenerator generator = new ElevatorTrafficGenerator(controller, lowestFloor, highestFloor);
-        generator.start();
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("SHUT IT DOWN!");
-            runner.interrupt();
-            generator.interrupt();
-        }));
-    }
-
-    private static int getIntegerInput(String prompt) {
-        System.out.println(prompt);
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            try {
-                return scanner.nextInt();
-            } catch (InputMismatchException ex) {
-                System.out.println("Please enter a number");
-            }
-        }
     }
 }
